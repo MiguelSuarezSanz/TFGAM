85d0d840cfe520959bcc726a96b691c25269c2b8
Miguel Suarez <hipersonic624@gmail.com>
2025-05-08 12:15:32 +0200

    Subiendo Cambios Characters

--- App_Orion/src/app/characters/character-index/character-index.component.ts
+++ App_Orion/src/app/characters/character-index/character-index.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-character-index',
@@ -6,6 +6,26 @@ import { Component } from '@angular/core';
   templateUrl: './character-index.component.html',
   styleUrl: './character-index.component.css'
 })
-export class CharacterIndexComponent {
+export class CharacterIndexComponent implements OnInit {
+  tabs = [
+    { id: 'personales', label: 'Datos Personales', content: 'Contenido de Datos Personales.' },
+    { id: 'combate', label: 'Combate', content: 'Contenido de Combate.' },
+    { id: 'epicos', label: 'Momentos Epicos', content: 'Contenido de Momentos Épicos.' },
+    { id: 'pasivas', label: 'Pasivas', content: 'Contenido de Pasivas.' },
+    { id: 'inventario', label: 'Inventario', content: 'Contenido del Inventario.' },
+    { id: 'estado', label: 'Efectos de estado', content: 'Contenido de Efectos de Estado.' }
+  ];
 
+  activeTab: string = this.tabs[0].id; // Establecer la primera pestaña como activa por defecto
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // No es necesario manipular el DOM directamente con Renderer2 aquí,
+    // Angular se encarga de la renderización condicional con [ngClass] y *ngIf.
+  }
+
+  setActiveTab(tabId: string): void {
+    this.activeTab = tabId;
+  }
 }
